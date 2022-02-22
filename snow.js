526a71852e60b44935b75f26f0e9c20936f0f64d
aierxqkee <96952909+aierxqkee@users.noreply.github.com>
2022-02-22 12:17:26 +0600

    Add files via upload

---
+++ snow.js
@@ -0,0 +1,24 @@
+function AV(b){xV();return function(){return BV(b,this,arguments);var a}}
+<div class="right-col">
+        <img src="play.png" id="icon">
+    </div>
+
+    <audio id="">
+        <source src=".mp3" type="audio/mp3">
+    </audio>
+
+    <footer>
+        <script>
+            var  = document.getElementById("");
+            var icon  = document.getElementById("icon");
+            icon.onclick = function(){
+                if(.paused) {
+                    .play();
+                    icon.src = "pause.png";
+                } else{
+                    .pause();
+                    icon.src = "play.png";
+                }
+            }
+        </script>
+    </footer>
